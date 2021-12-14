f19382ab690c518a965adddfa6aa79c6c34d4a35
amineohn <amineprojet7@gmail.com>
2021-12-14 13:45:58 +0100

    color button

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -68,7 +68,7 @@ const Hello = () => {
                   <button
                     type="button"
                     onClick={() => setError("")}
-                    className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                    className="py-2 px-4 text-sm font-semibold bg-red-50 text-red-700 hover:bg-violet-100 w-full transition ease-in duration-200 text-center shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                   >
                     Close
                   </button>
@@ -101,7 +101,7 @@ const Hello = () => {
                   <button
                     type="button"
                     onClick={() => setSuccess("")}
-                    className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                    className="py-2 px-4 text-sm font-semibold bg-green-50 text-green-700 hover:bg-green-100 w-full transition ease-in duration-200 text-center shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                   >
                     Close
                   </button>
